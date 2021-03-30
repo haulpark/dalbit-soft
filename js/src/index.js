7c363159708209d86ee5114ac576fcf11249f24d
CHO KANG RAE <joh971128@gmail.com>
2021-03-30 18:10:41 +0900

    index js 수정 및 warning page 정렬 수정

--- js/src/index.js
+++ js/src/index.js
@@ -11,6 +11,6 @@
     function openNewWindow(window) {
       open (window,"NewWindow","left=0, top=0, toolbar=no, location=no,\
             directories=no, status=no, menubar=no, scrollbars=no\
-            resizable=yes, width=479, height=700");}
+            resizable=yes, width=478, height=700");}
   }
  
