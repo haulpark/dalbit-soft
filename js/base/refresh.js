8e5fab59345b425ed8ef0cabcce053f96648be2a
CHO KANG RAE <joh971128@gmail.com>
2021-04-26 11:21:29 +0900

    새로고침 이벤트 수정

--- js/base/refresh.js
+++ js/base/refresh.js
@@ -1,15 +1,14 @@
 //refresh.js
 //새로고침 차단
 
-function noEvent() {
+function noEvent() { // 새로 고침 방지
   if (event.keyCode == 116) {
-  event.keyCode= 2;
-  return false;
+      alert("새로고침을 할 수 없습니다.");
+      event.keyCode = 2;
+      return false;
+  } else if (event.ctrlKey
+          && (event.keyCode == 78 || event.keyCode == 82)) {
+      return false;
   }
-  else if(event.ctrlKey && (event.keyCode==78 || event.keyCode == 82))
-  {
-  return false;
-  }
-  }
-  
-  document.onkeydown = noEvent;
+}
+document.onkeydown = noEvent;
